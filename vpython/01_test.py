1b1b0fbfd5d0363d2e0d8131f9ceea85af21c60a
guozhenjiang <guo_zhen_jiang@163.com>
2020-04-02 18:09:15 +0800

    IDL 界面基本确定，信号、槽机制有进展。

--- vpython/01_test.py
+++ vpython/01_test.py
@@ -1,32 +1,41 @@
-# # from visual import *
-# from vpython import *
-
-# sphere()
-
-# from vpython import sphere,vector
-# L = 5
-# R = 0.3
-# for i in range(-L,L+1):
-#     for j in range(-L,L+1):
-#         for k in range(-L,L+1):
-#             sphere(pos=vector(i,j,k),radius=R)
-
-
-
-from pyqtgraph.Qt import QtGui, QtCore
-# import numpy as np
-import pyqtgraph as pg
-# from pyqtgraph.ptime import time
-# from scipy import signal
-# import pyqtgraph.widgets.RemoteGraphicsView
-
-app = QtGui.QApplication([])
-
-win = pg.GraphicsWindow(title = '室内定位点到点数据滤波')
-win.title = "hello"
-win.resize(1000, 600)
-
-pg.setConfigOptions(antialias = True)
-
 from vpython import *
-ball = sphere(pos=vector(1,2,1), radius=0.5)
+#GlowScript 2.9 VPython
+scene.width = scene.height = 600
+scene.background = color.white
+scene.range = 1.3
+N = 10
+scene.title = 'A {}-element points object with random radii and colors'.format(N)
+# Display frames per second and render time:
+scene.append_to_title("<div id='fps'/>")
+
+run = True
+def Runbutton(b):
+    global run
+    if b.text == 'Pause':
+        run = False
+        b.text = 'Run'
+    else:
+        run = True
+        b.text = 'Pause'
+
+button(text='Pause', bind=Runbutton)
+scene.append_to_caption("""
+To rotate "camera", drag with right button or Ctrl-drag.
+To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
+  On a two-button mouse, middle is left + right.
+To pan left/right and up/down, Shift-drag.
+Touch screen: pinch/extend to zoom, swipe or two-finger rotate.""")
+
+p = []
+last = vec(0,0,0)
+for i in range(N):
+    next = last+0.1*vec.random()
+    while mag(next) > 1: # if next is outside the sphere, try another random value
+        next = last+0.1*vec.random()
+    p.append({'pos':next, 'radius':0.002+0.04*random(), 'color':(vec(1,1,1)+vec.random())/2})
+    last = next
+c = points(pos=p, size_units='world')
+while True:
+    rate(60)
+    if run: # Currently there isn't a way to rotate a points object, so rotate scene.forward:
+        scene.forward = scene.forward.rotate(angle=-0.005, axis=vec(0,1,0))
